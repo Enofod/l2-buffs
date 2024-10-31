2f4764c4ec57fa2411cefc50f0f70ebb7daefe79
Enofod <enofod@gmail.com>
2024-10-31 14:05:39 +0100

    Init

--- src/app/buffs/buffs.component.ts
+++ src/app/buffs/buffs.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { MAGE_NEWBIE_GUIDE_BUFFS, WARRIOR_NEWBIE_GUIDE_BUFFS } from './buff/newbie-guide-buffts';
 
 @Component({
   selector: 'app-buffs',
   standalone: true,
-  imports: [],
+  imports: [MatTooltipModule],
   templateUrl: './buffs.component.html',
   styleUrl: './buffs.component.scss'
 })
 export class BuffsComponent {
 
-  levelsToIterate = Array.from({length: 18}, (_, i) => i + 7)
+  levelsToIterate = Array.from({length: 17}, (_, i) => i + 8)
 
   WARRIOR_NG = WARRIOR_NEWBIE_GUIDE_BUFFS
   MAGE_NG = MAGE_NEWBIE_GUIDE_BUFFS
